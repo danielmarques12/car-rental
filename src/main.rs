020e00e859fbd14d048b214598462ada77043e82
Daniel Marques <daniel.brz2009@gmail.com>
2021-08-24 00:32:54 -0300

    Using sqlite in Rust for the first time!!!!

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,51 @@
+use rusqlite::{Connection, Result};
+
+#[derive(Debug)]
 struct Car {
+    id: u8,
     model: String,
     brand: String,
+    plate: String,
     price: f32,
     year: u16,
 }
 
-fn main() {
-    let mut cars_list: Vec<Car> = Vec::new();
+fn main() -> Result<()> {
+    let conn = Connection::open("cars.db")?;
 
-    let car = Car {
-        model: String::from("sei la"),
-        brand: String::from("sei la"),
-        price: 2022.2,
-        year: 2022,
-    };
+    conn.execute(
+        "create table if not exists cars (
+            id integer primary key autoincrement,
+            model text not null,
+            brand text not null,
+            plate text not null,
+            price real not null,
+            year integer not null
+        )",
+        [],
+    )?;
 
-    cars_list.push(car);
+    let mut stmt = conn.prepare("SELECT * FROM cars;")?;
 
-    // let cars = cars_list.iter();
+    let cars = stmt.query_map([], |row| {
+        Ok(Car {
+            id: row.get(0)?,
+            model: row.get(1)?,
+            brand: row.get(2)?,
+            plate: row.get(3)?,
+            price: row.get(4)?,
+            year: row.get(5)?,
+        })
+    })?;
 
-    for car in cars_list {
-        println!("Model: {}", car.model);
+    for car in cars {
+        println!("Car specs: {:?}", car);
     }
+
+    Ok(())
 }
+
+// fn list_all_cars() {}
+// fn sum_all_cars_prices() {}
+// fn list_car_by_plate() {}
+// fn rent_car() {}
